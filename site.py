5df93bf7e87e8ae1b49f33e97afc142ac92286cf
Omar Sahboun <omar.lilian.sahboun@gmail.com>
2022-08-11 09:50:40 +0200

    Modification du site

--- site.py
+++ site.py
@@ -1,13 +1,6 @@
-
 import streamlit as st
 import pandas as pd
-
-
-
 from sklearn.model_selection import train_test_split
-
-
-
 from sklearn.metrics import *
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
